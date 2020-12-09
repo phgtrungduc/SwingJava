1b92342cef58ec2967ce12edc8bd416022fb85a3
Phương Trung Đức <phgtrungduc@gmail.com>
2020-12-09 10:57:11 +0700

    xong giao dien

--- src/main/RentingBike.java
+++ src/main/RentingBike.java
@@ -17,6 +17,7 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
@@ -25,7 +26,7 @@ public class RentingBike extends JDialog {
 	private final JPanel contentPanel = new JPanel();
 	public RentingBike() {
 		setResizable(false);
-		setTitle("Thu� xe");
+		setTitle("Thuê xe");
 		setBounds(100, 100, 800, 500);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -34,7 +35,7 @@ public class RentingBike extends JDialog {
 		
 		JPanel rentingBikePanel = new JPanel();
 		rentingBikePanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
-		rentingBikePanel.setBounds(10, 11, 760, 440);
+		rentingBikePanel.setBounds(10, 10, 760, 440);
 		contentPanel.add(rentingBikePanel);
 		rentingBikePanel.setLayout(null);
 		
@@ -47,9 +48,9 @@ public class RentingBike extends JDialog {
 		{
 			JPanel imgPanel = new JPanel();
 			imgPanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
-			imgPanel.setBounds(10, 25, 125, 88);
+			imgPanel.setBounds(5, 5, 130, 120);
 			rentingBikePanel.add(imgPanel);
-			imgPanel.setLayout(null);
+			imgPanel.setLayout(new BorderLayout());
 			{
 				JLabel imgLabel = new JLabel("");
 				imgLabel.setBounds(10, 11, 106, 66);
@@ -57,21 +58,133 @@ public class RentingBike extends JDialog {
 				        Image.SCALE_SMOOTH);
 				ImageIcon imageIcon = new ImageIcon(dimg);
 				imgLabel.setIcon(imageIcon);	
+				imgLabel.setVerticalAlignment(JLabel.CENTER);
+				imgLabel.setHorizontalAlignment(JLabel.CENTER);
 				imgPanel.add(imgLabel);
 			}
 		}
-		JLabel title = new JLabel("Cho thu� xe");
-		rentingBikePanel.add(title);
-		title.setBounds(0,0,50,50);
-		JPanel titlePanel = new JPanel(null);
-		titlePanel.add(title);
-		contentPanel.add(titlePanel);
-		titlePanel.setBounds(135,0,500,200);
-		titlePanel.setBackground(Color.BLUE);
 		
+		JLabel title = new JLabel("Cho thuê xe");
+		
+		
+		JPanel titlePanel = new JPanel();
+		titlePanel.add(title);	
+		title.setHorizontalAlignment(JLabel.CENTER);
+		title.setVerticalAlignment(JLabel.BOTTOM);
+		title.setFont(new Font("Verdana", Font.BOLD, 50));
+		
+		rentingBikePanel.add(titlePanel);
+		titlePanel.setBounds(140,5,610,120);
+		titlePanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		titlePanel.setLayout(new BorderLayout());
+		titlePanel.add(title);
+		
+		JPanel upcontentPanel = new JPanel();
+		rentingBikePanel.add(upcontentPanel);
+		upcontentPanel.setBounds(5,130,750,50);
+//		upcontentPanel.setBackground(Color.BLACK);
+		upcontentPanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		
+		JLabel inputCodeLabel = new JLabel("Nhập mã xe");
+		upcontentPanel.add(inputCodeLabel);
+		inputCodeLabel.setFont(new Font("Verdana", Font.BOLD, 20));
+		inputCodeLabel.setHorizontalAlignment(JLabel.CENTER);
+		
+		JTextField inputCodeField = new JTextField();
+		inputCodeField.setPreferredSize( new Dimension( 150, 30 ) );
+		upcontentPanel.add(inputCodeField);
+		
+		JButton codeButton = new JButton("Tìm kiếm xe");
+		upcontentPanel.add(codeButton);
+		
+		JPanel downContentPanel = new JPanel(null);
+		rentingBikePanel.add(downContentPanel);
+		downContentPanel.setBounds(5,190,750,250);
+		downContentPanel.setBorder(new LineBorder(Color.LIGHT_GRAY));
+		
+		
+//		downContentPanel.add(nameLabel);
+//		downContentPanel.add(nameField);
+//		nameLabel.setBounds(300,0,50,50);
+//		nameField.setBounds(360,5,150,30);
+//		nameField.setPreferredSize( new Dimension( 150, 30 ) );
+//		
+//		
+//		JLabel typeLabel = new JLabel("Tên xe");
+//		
+//		
+//		JTextField typeField = new JTextField();
+//		typeField.setPreferredSize( new Dimension( 150, 30 ) );
+//		downContentPanel.add(typeLabel);
+//		downContentPanel.add(typeField);
+		
+		JPanel namePanel = new JPanel();
+		downContentPanel.add(namePanel);
+		namePanel.setBounds(5,5,740,40);
+		
+		JLabel nameLabel = new JLabel("Tên xe");
+		JTextField nameField = new JTextField();
+		nameLabel.setHorizontalAlignment(JLabel.CENTER);
+		namePanel.add(nameLabel);
+		namePanel.add(nameField);
+		nameField.setPreferredSize( new Dimension( 150, 30 ) );
+		
+		
+		JPanel typePanel = new JPanel();
+		downContentPanel.add(typePanel);
+		typePanel.setBounds(5,45,740,40);
+		
+		JLabel typeLabel = new JLabel("Loại xe");
+		JTextField typeField = new JTextField();
+		typeLabel.setHorizontalAlignment(JLabel.CENTER);
+		typePanel.add(typeLabel);
+		typePanel.add(typeField);
+		typeField.setPreferredSize( new Dimension( 150, 30 ) );
+		
+		
+		JPanel weightPanel = new JPanel();
+		downContentPanel.add(weightPanel);
+		weightPanel.setBounds(5,90,740,40);
+		
+		JLabel weightLabel = new JLabel("Trọng lượng");
+		JTextField weightField = new JTextField();
+		weightLabel.setHorizontalAlignment(JLabel.CENTER);
+		weightPanel.add(weightLabel);
+		weightPanel.add(weightField);
+		weightField.setPreferredSize( new Dimension( 150, 30 ) );
+		
+		JPanel licensePanel = new JPanel();
+		downContentPanel.add(licensePanel);
+		licensePanel.setBounds(5,140,740,40);
+		
+		JLabel licenseLabel = new JLabel("Biển số xe");
+		JTextField licenseField = new JTextField();
+		licenseLabel.setHorizontalAlignment(JLabel.CENTER);
+		licensePanel.add(licenseLabel);
+		licensePanel.add(licenseField);
+		licenseField.setPreferredSize( new Dimension( 150, 30 ) );
+		
+		JPanel datePanel = new JPanel();
+		downContentPanel.add(datePanel);
+		datePanel.setBounds(5,190,740,40);
+		
+		JLabel dateLabel = new JLabel("Ngày sản xuất");
+		JTextField dateField = new JTextField();
+		dateLabel.setHorizontalAlignment(JLabel.CENTER);
+		datePanel.add(dateLabel);
+		datePanel.add(dateField);
+		dateField.setPreferredSize( new Dimension( 150, 30 ) );
+		JButton rentingButton = new JButton("Thuê xe");
+		datePanel.add(rentingButton);
+//		JLabel licenseLabel = new JLabel("Tên xe");
+//		JTextField licenseField = new JTextField();
+//		licenseField.setPreferredSize( new Dimension( 150, 30 ) );
+//		downContentPanel.add(licenseLabel);
+//		downContentPanel.add(licenseField);
+//		
 	}
 	public static void main(String[] args) {
-		RentingBikeScreen scr=  new RentingBikeScreen();
+		RentingBike scr=  new RentingBike();
 		scr.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 		scr.setVisible(true);
 	}
